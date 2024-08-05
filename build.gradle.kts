017ddcef08f0b802d3df1567f0472426fedd1efc
Bram <bram@bramdekker.com>
2024-08-05 16:39:42 +0900

    Rewrote the build chain from Maven to Gradle Added: GitHub Build Workflow Added: RunPaper to easily boot up a test environment

---
+++ build.gradle.kts
@@ -0,0 +1,79 @@
+plugins {
+    `java-library`
+    `maven-publish`
+    alias(libs.plugins.run.paper)
+}
+
+repositories {
+    mavenLocal()
+    maven {
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+java {
+    disableAutoTargetJvm()
+    toolchain.languageVersion = JavaLanguageVersion.of(21)
+}
+
+dependencies {
+    compileOnly(libs.paper.api)
+}
+
+group = "de.outdev.totemguardv2"
+version = "1.0"
+description = "TotemGuardV2"
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+    }
+}
+
+tasks {
+    withType<JavaCompile> {
+        options.encoding = Charsets.UTF_8.name()
+        options.release = 17
+    }
+
+    withType<Javadoc>() {
+        options.encoding = Charsets.UTF_8.name()
+    }
+
+    processResources {
+        inputs.property("version", project.version)
+        filesMatching("plugin.yml") {
+            expand("version" to project.version)
+        }
+    }
+
+    defaultTasks("build")
+
+    // 1.8.8 - 1.16.5 = Java 8
+    // 1.17           = Java 16
+    // 1.18 - 1.20.4  = Java 17
+    // 1-20.5+        = Java 21
+    val version = "1.20.4"
+    val javaVersion = JavaLanguageVersion.of(17)
+
+    val jvmArgsExternal = listOf(
+        "-Dcom.mojang.eula.agree=true"
+    )
+
+    runServer {
+        minecraftVersion(version)
+        runDirectory = rootDir.resolve("run/paper/$version")
+
+        javaLauncher = project.javaToolchains.launcherFor {
+            languageVersion = javaVersion
+        }
+
+        downloadPlugins {
+            url("https://github.com/EssentialsX/Essentials/releases/download/2.20.1/EssentialsX-2.20.1.jar")
+            url("https://download.luckperms.net/1552/bukkit/loader/LuckPerms-Bukkit-5.4.137.jar")
+            url("https://ci.lucko.me/job/spark/439/artifact/spark-bukkit/build/libs/spark-1.10.93-bukkit.jar")
+        }
+
+        jvmArgs = jvmArgsExternal
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+rootProject.name = "TotemGuardV2"
