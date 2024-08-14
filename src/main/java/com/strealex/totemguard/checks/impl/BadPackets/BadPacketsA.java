d7c5ff1dfb5252ec673e163d971925257418b7d9
Asleepp <119438940+Asleeepp@users.noreply.github.com>
2024-08-14 18:57:05 -0300

    Method movearound again

--- src/main/java/com/strealex/totemguard/checks/impl/BadPackets/BadPacketsA.java
+++ src/main/java/com/strealex/totemguard/checks/impl/BadPackets/BadPacketsA.java
@@ -26,15 +26,15 @@ public class BadPacketsA extends Check implements PacketListener {
             return;
         }
 
+        final Settings.Checks.BadPacketsA settings = plugin.getConfigManager().getSettings().getChecks().getBadPacketsA();
+        if (!settings.isEnabled()) {
+            return;
+        }
+
         WrapperPlayClientPluginMessage packet = new WrapperPlayClientPluginMessage(event);
         String channel = packet.getChannelName();
 
         if (channel.equalsIgnoreCase("minecraft:using_autototem")) {
-            final Settings.Checks.BadPacketsA settings = plugin.getConfigManager().getSettings().getChecks().getBadPacketsA();
-            if (!settings.isEnabled()) {
-                return;
-            }
-
             Component checkDetails = Component.text()
                     .append(Component.text("Channel: ", NamedTextColor.GRAY))
                     .append(Component.text(channel, NamedTextColor.GOLD))
