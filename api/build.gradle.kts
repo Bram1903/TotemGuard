933f9a7dcc28b2dfa94cb38da77f0263c702b383
Bram <bram@bramdekker.com>
2024-12-10 20:45:54 +0100

    Improved: API jar naming

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    alias(libs.plugins.shadow)
 }
 
 group = "com.deathmotion.totemguard.api"
@@ -30,8 +31,17 @@ java {
 }
 
 tasks {
+    shadowJar {
+        archiveFileName = "${rootProject.name}API.jar"
+        archiveClassifier = null
+    }
+
+    assemble {
+        dependsOn(shadowJar)
+    }
+
     javadoc {
-        title = "TotemGuardAPI v${rootProject.version}"
+        title = "TotemGuardAPI v${rootProject.ext["versionNoHash"]}"
         options.encoding = Charsets.UTF_8.name()
         options {
             (this as CoreJavadocOptions).addBooleanOption("Xdoclint:none", true)
