9ed05da1b491a28a756963af1ba02cc927c2eb76
Bram <bram@bramdekker.com>
2024-12-13 20:49:34 +0100

    Added: GPL3 License Headers to Inventory A & B

--- src/main/java/com/deathmotion/totemguard/checks/impl/inventory/InventoryB.java
+++ src/main/java/com/deathmotion/totemguard/checks/impl/inventory/InventoryB.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of TotemGuard - https://github.com/Bram1903/TotemGuard
+ * Copyright (C) 2024 Bram and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.deathmotion.totemguard.checks.impl.inventory;
 
 import com.deathmotion.totemguard.TotemGuard;
@@ -51,18 +69,20 @@ public final class InventoryB extends Check implements PacketListener {
         }
 
     }
+
     @Override
-    public void resetData () {
+    public void resetData() {
         super.resetData();
         inventoryClick.clear();
     }
 
     @Override
-    public void resetData (UUID uuid){
+    public void resetData(UUID uuid) {
         super.resetData(uuid);
         inventoryClick.remove(uuid);
 
     }
+
     private void check(Player player, long storedTime, String action) {
         long timeDifference = Math.abs(System.currentTimeMillis() - storedTime);
 
