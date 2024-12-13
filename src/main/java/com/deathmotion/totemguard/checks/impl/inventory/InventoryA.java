437c875885a54a63bfe2bb75b2acb6fbc79a9a73
OutDev <lukassinah@icloud.com>
2024-12-13 22:07:22 +0100

    Added: GPL3 License Headers to Inventory A

--- src/main/java/com/deathmotion/totemguard/checks/impl/inventory/InventoryA.java
+++ src/main/java/com/deathmotion/totemguard/checks/impl/inventory/InventoryA.java
@@ -1,10 +1,26 @@
+/*
+ * This file is part of TotemGuard - https://github.com/Bram1903/TotemGuard
+ * Copyright (C) 2024 Bram and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.deathmotion.totemguard.checks.impl.inventory;
 
 import com.deathmotion.totemguard.TotemGuard;
 import com.deathmotion.totemguard.checks.Check;
 import com.deathmotion.totemguard.config.Settings;
-import com.deathmotion.totemguard.models.PlayerState;
-import com.deathmotion.totemguard.models.TotemPlayer;
 import com.deathmotion.totemguard.util.MessageService;
 import com.deathmotion.totemguard.util.datastructure.Pair;
 import com.github.retrooper.packetevents.event.PacketListener;
@@ -13,9 +29,6 @@ import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.entity.Player;
-
-import java.util.Optional;
-import java.util.UUID;
 
 public final class InventoryA extends Check implements PacketListener {
 
