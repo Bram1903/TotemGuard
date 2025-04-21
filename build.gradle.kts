3abaa58106d01e85c04f2fe0697b91ece3f8faa0
Bram <bram@bramdekker.com>
2025-04-21 16:33:39 +0200

    Changed: runPaper task to 1.21.4 again

--- build.gradle.kts
+++ build.gradle.kts
@@ -90,8 +90,8 @@ tasks {
     // 1.17           = Java 16
     // 1.18 - 1.20.4  = Java 17
     // 1-20.5+        = Java 21
-    val version = "1.20"
-    val javaVersion = JavaLanguageVersion.of(17)
+    val version = "1.21.4"
+    val javaVersion = JavaLanguageVersion.of(21)
 
     val jvmArgsExternal = listOf(
         "-Dcom.mojang.eula.agree=true"
