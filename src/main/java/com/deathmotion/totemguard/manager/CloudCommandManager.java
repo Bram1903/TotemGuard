a54f59964236e751752f61ac154e019ffe094669
Bram <bram@bramdekker.com>
2025-09-30 15:04:30 +0200

    Force brigadier command execution

--- src/main/java/com/deathmotion/totemguard/manager/CloudCommandManager.java
+++ src/main/java/com/deathmotion/totemguard/manager/CloudCommandManager.java
@@ -3,9 +3,12 @@ package com.deathmotion.totemguard.manager;
 import com.deathmotion.totemguard.TotemGuard;
 import com.deathmotion.totemguard.commands.cloud.CommandBuilder;
 import org.bukkit.command.CommandSender;
+import org.incendo.cloud.brigadier.BrigadierSetting;
+import org.incendo.cloud.brigadier.CloudBrigadierManager;
 import org.incendo.cloud.bukkit.CloudBukkitCapabilities;
 import org.incendo.cloud.execution.ExecutionCoordinator;
 import org.incendo.cloud.paper.LegacyPaperCommandManager;
+import org.incendo.cloud.setting.Configurable;
 
 public class CloudCommandManager {
     private final LegacyPaperCommandManager<CommandSender> commandManager;
@@ -18,6 +21,9 @@ public class CloudCommandManager {
 
         if (commandManager.hasCapability(CloudBukkitCapabilities.NATIVE_BRIGADIER)) {
             commandManager.registerBrigadier();
+            CloudBrigadierManager<CommandSender, ?> cbm = commandManager.brigadierManager();
+            Configurable<BrigadierSetting> settings = cbm.settings();
+            settings.set(BrigadierSetting.FORCE_EXECUTABLE, true);
             plugin.getLogger().info("Hooked into Brigadier for native command support");
         } else if (commandManager.hasCapability(CloudBukkitCapabilities.ASYNCHRONOUS_COMPLETION)) {
             commandManager.registerAsynchronousCompletions();
