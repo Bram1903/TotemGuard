076280c5462db6788b9014f9433e3145b42b3d0a
Bram <bram@bramdekker.com>
2025-10-04 12:48:04 +0200

    Refactored the CloudCommandManager

--- src/main/java/com/deathmotion/totemguard/manager/CloudCommandManager.java
+++ src/main/java/com/deathmotion/totemguard/manager/CloudCommandManager.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of TotemGuard - https://github.com/Bram1903/TotemGuard
+ * Copyright (C) 2025 Bram and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.deathmotion.totemguard.manager;
 
 import com.deathmotion.totemguard.TotemGuard;
@@ -9,27 +27,49 @@ import org.incendo.cloud.bukkit.CloudBukkitCapabilities;
 import org.incendo.cloud.execution.ExecutionCoordinator;
 import org.incendo.cloud.paper.LegacyPaperCommandManager;
 import org.incendo.cloud.setting.Configurable;
+import org.jetbrains.annotations.NotNull;
 
-public class CloudCommandManager {
-    private final LegacyPaperCommandManager<CommandSender> commandManager;
+import java.util.logging.Logger;
 
-    public CloudCommandManager(TotemGuard plugin) {
-        commandManager = LegacyPaperCommandManager.createNative(
-                plugin,
-                ExecutionCoordinator.simpleCoordinator()
-        );
+public final class CloudCommandManager {
 
+    private final @NotNull Logger log;
+    private final @NotNull LegacyPaperCommandManager<CommandSender> commandManager;
+
+    public CloudCommandManager(@NotNull TotemGuard plugin) {
+        this.log = plugin.getLogger();
+        this.commandManager = LegacyPaperCommandManager.createNative(plugin, ExecutionCoordinator.simpleCoordinator());
+
+        log.info(() -> "Initializing command manager...");
+        configureCapabilities();
+        registerCommands();
+    }
+
+    private void configureCapabilities() {
         if (commandManager.hasCapability(CloudBukkitCapabilities.NATIVE_BRIGADIER)) {
-            commandManager.registerBrigadier();
-            CloudBrigadierManager<CommandSender, ?> cbm = commandManager.brigadierManager();
-            Configurable<BrigadierSetting> settings = cbm.settings();
-            settings.set(BrigadierSetting.FORCE_EXECUTABLE, true);
-            plugin.getLogger().info("Hooked into Brigadier for native command support");
-        } else if (commandManager.hasCapability(CloudBukkitCapabilities.ASYNCHRONOUS_COMPLETION)) {
-            commandManager.registerAsynchronousCompletions();
-            plugin.getLogger().info("Asynchronous command completions support enabled");
+            registerBrigadier();
+        } else {
+            log.info(() -> "Brigadier not available on this platform.");
         }
 
+        if (commandManager.hasCapability(CloudBukkitCapabilities.ASYNCHRONOUS_COMPLETION)) {
+            commandManager.registerAsynchronousCompletions();
+            log.info(() -> "Asynchronous command completions enabled.");
+        }
+    }
+
+    private void registerBrigadier() {
+        commandManager.registerBrigadier();
+
+        final CloudBrigadierManager<CommandSender, ?> brigadier = commandManager.brigadierManager();
+        final Configurable<BrigadierSetting> settings = brigadier.settings();
+
+        settings.set(BrigadierSetting.FORCE_EXECUTABLE, true);
+
+        log.info(() -> "Hooked into Brigadier for native command support.");
+    }
+
+    private void registerCommands() {
         new CommandBuilder(commandManager);
     }
 }
