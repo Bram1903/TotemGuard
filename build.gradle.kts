5639dd7933c8ac9f012549feb10c46147be39512
Bram <bram@bramdekker.com>
2025-10-07 09:30:45 +0200

    Updated mc version

--- build.gradle.kts
+++ build.gradle.kts
@@ -123,7 +123,7 @@ tasks {
     // 1.17           = Java 16
     // 1.18 - 1.20.4  = Java 17
     // 1-20.5+        = Java 21
-    val version = "1.21.8"
+    val version = "1.21.9"
     val javaVersion = JavaLanguageVersion.of(21)
 
     val jvmArgsExternal = listOf(
