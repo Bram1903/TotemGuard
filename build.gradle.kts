0c72b0a30fe62184ee42fc9c28ed65ef4e5eb6af
Bram <bram@bramdekker.com>
2025-10-26 12:26:42 +0100

    Bumped plugin version from 2.1.1-SNAPSHOT -> 2.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
 group = "com.deathmotion.totemguard"
 description = "TotemGuard is a simple anti-cheat that tries to detect players who are using AutoTotem."
 val fullVersion = "2.1.1"
-val snapshot = true
+val snapshot = false
 
 fun getVersionMeta(includeHash: Boolean): String {
     if (!snapshot) {
