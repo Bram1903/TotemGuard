01ca8dbfc4991d9153218f96cb4aa9e67a52fcda
Bram <bram@bramdekker.com>
2026-04-01 20:10:06 +0200

    Removed: TickB (not really needed anymore)

--- common/src/main/java/com/deathmotion/totemguard/common/check/CheckManagerImpl.java
+++ common/src/main/java/com/deathmotion/totemguard/common/check/CheckManagerImpl.java
@@ -29,7 +29,6 @@ import com.deathmotion.totemguard.common.check.impl.inventory.InventoryC;
 import com.deathmotion.totemguard.common.check.impl.mods.Mod;
 import com.deathmotion.totemguard.common.check.impl.protocol.*;
 import com.deathmotion.totemguard.common.check.impl.tick.TickA;
-import com.deathmotion.totemguard.common.check.impl.tick.TickB;
 import com.deathmotion.totemguard.common.check.type.EventCheck;
 import com.deathmotion.totemguard.common.check.type.ExtendedCheck;
 import com.deathmotion.totemguard.common.check.type.PacketCheck;
@@ -67,7 +66,6 @@ public class CheckManagerImpl {
                 .put(ProtocolE.class, new ProtocolE(player))
                 .put(ProtocolF.class, new ProtocolF(player))
                 .put(TickA.class, new TickA(player))
-                .put(TickB.class, new TickB(player))
                 .put(InventoryA.class, new InventoryA(player))
                 .put(InventoryB.class, new InventoryB(player))
                 .put(InventoryC.class, new InventoryC(player))
