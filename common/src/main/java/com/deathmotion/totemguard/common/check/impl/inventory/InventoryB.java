35682d368b7b1043f7ec53941d48a34747e7ca2a
Bram <bram@bramdekker.com>
2026-04-17 12:02:16 +0200

    Fixed small typo in InventoryB

--- common/src/main/java/com/deathmotion/totemguard/common/check/impl/inventory/InventoryB.java
+++ common/src/main/java/com/deathmotion/totemguard/common/check/impl/inventory/InventoryB.java
@@ -50,7 +50,7 @@ public class InventoryB extends CheckImpl implements PacketCheck {
             if (data.isSprinting()) {
                 failInventory("click (sprinting");
             } else if (inputData.isInput()) {
-                failInventory("click (move");
+                failInventory("click (move)");
             }
         } else if (packetType == PacketType.Play.Client.CLOSE_WINDOW) {
             if (data.isInventoryMitigatedThisTick()) return;
