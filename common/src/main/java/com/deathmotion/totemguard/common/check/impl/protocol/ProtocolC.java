53c93fca5fdaa6f84250d9a27cfaeed5054295dc
Bram <bram@bramdekker.com>
2026-04-26 11:50:47 +0200

    Remove unneeded debug in ProtocolC

--- common/src/main/java/com/deathmotion/totemguard/common/check/impl/protocol/ProtocolC.java
+++ common/src/main/java/com/deathmotion/totemguard/common/check/impl/protocol/ProtocolC.java
@@ -44,7 +44,7 @@ public class ProtocolC extends CheckImpl implements PacketCheck {
         if (event.getPacketType() != PacketType.Play.Client.CLIENT_TICK_END) return;
 
         if (tickData.isAttacking() && tickData.isPlacing() && !tickData.isInteracting()) {
-            fail("attack/place");
+            fail();
         }
     }
 }
